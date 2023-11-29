f66ea505bf6ca2ea7d921547cccb41e586fc7593
Andreev Dmitriy <andreev.dp@bk.ru>
2023-11-29 05:01:45 -0800

    create keyboard

--- Sirius/main.py
+++ Sirius/main.py
@@ -2,9 +2,10 @@ import keyboard_menu
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram.filters import Command
 from datetime import datetime
 from config_reader import config
+from aiogram import F
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -22,10 +23,42 @@ bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 dp["started_at"] = datetime.now().strftime("%H:%M - %d/%m/%Y")
 
-# Хэндлер на команду /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!") 
+# # Хэндлер на команду /start
+# @dp.message(Command("start"))
+# async def cmd_start(F: types.Message):
+#     await F.answer("Hello!") 
+
+
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(F: types.Message):
+    await F.reply("Test 1")
+
+
+
+# Хэндлер на команду /test2
+@dp.message(Command("test2"))
+async def cmd_test2(F: types.Message):
+    await F.reply("Test 2")
+
+
+@dp.message(Command("answer"))
+async def cmd_answer(F: types.Message):
+    await F.answer("Простой ответ")  
+
+
+@dp.message(Command("бросить кубик"))
+async def cmd_dice(F: types.Message):
+    await F.answer_dice(emoji="🎲")
+
+
+@dp.message(Command("info"))
+async def cmd_info(F: types.Message, started_at: str):
+    await F.answer(f"Бот непрерывно трудится для вас с   {started_at}")
+
+@dp.message(Command("name"))
+async def cmd_name(Message: types.Message):
+    await Message.answer(f"Привет {Message.from_user.full_name}")
 
 
 # Запуск процесса поллинга новых апдейтов
@@ -33,33 +66,36 @@ async def main():
     await dp.start_polling(bot)
 
 
-# Хэндлер на команду /test1
-@dp.message(Command("test1"))
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Получить заметку"), types.KeyboardButton(text="Создать заметку")],
+        [types.KeyboardButton(text="Просмотр заметок")],
+        [types.KeyboardButton(text="Сколько я живу"), types.KeyboardButton(text="Кубик"), types.KeyboardButton(text="Пожаловаться")],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите вариант ответа:"
+    )
+    await message.answer("start", reply_markup=keyboard)
 
 
+@dp.message(F.text.lower() == "Кубик")
+async def with_puree(message: types.Message):
+    await message.reply("Отличный выбор!")
 
-# Хэндлер на команду /test2
-@dp.message(Command("test2"))
-async def cmd_test2(message: types.Message):
-    await message.reply("Test 2")
+@dp.message(F.text.lower() == "Получить заметку")
+async def selected_menu_main_1(message: types.Message):
+    await message.reply("1")
 
+@dp.message(F.text.lower() == "Создать заметку")
+async def selected_menu_main_2(message: types.Message):
+    await message.reply("2")
 
-@dp.message(Command("answer"))
-async def cmd_answer(message: types.Message):
-    await message.answer("Простой ответ")  
-
-
-@dp.message(Command("dice"))
-async def cmd_dice(message: types.Message):
-    await message.answer_dice(emoji="🎲")
-
-
-@dp.message(Command("info"))
-async def cmd_info(message: types.Message, started_at: str):
-    await message.answer(f"Бот непрерывно трудится для вас с   {started_at}")
-
+@dp.message(F.text.lower() == "Просмотр заметок")
+async def selected_menu_main_3(message: types.Message):
+    await message.reply("3")
 
 if __name__ == "__main__":
     asyncio.run(main())
