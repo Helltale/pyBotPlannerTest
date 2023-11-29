c359044b6be0fc48c6f2e3e0abd1a0f8fe4d1d42
Andreev Dmitriy <andreev.dp@bk.ru>
2023-11-29 02:27:34 -0800

    remove config.py, add .env and config_reader.py

--- Sirius/main.py
+++ Sirius/main.py
@@ -1,16 +1,23 @@
-import config
 import keyboard_menu
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from datetime import datetime
+from config_reader import config
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
+
+
 # Объект бота
-bot = Bot(token = config.BOT_TOKEN)
+# Для записей с типом Secret* необходимо 
+# вызывать метод get_secret_value(), 
+# чтобы получить настоящее содержимое вместо '*******'
+bot = Bot(token=config.bot_token.get_secret_value())
+
+
 # Диспетчер
 dp = Dispatcher()
 dp["started_at"] = datetime.now().strftime("%H:%M - %d/%m/%Y")
@@ -56,6 +63,4 @@ async def cmd_info(message: types.Message, started_at: str):
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
-    
     
