0471bc79fc75757906900468e25ee9a663a62b6e
Andreev Dmitriy <andreev.dp@bk.ru>
2023-11-29 04:15:32 +0400

    rewrite main.py logic

--- Sirius/main.py
+++ Sirius/main.py
@@ -1,21 +1,53 @@
 import config
+import asyncio
 import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
-from aiogram import Bot, Dispatcher, executor, types
 
-# Задаем уровень логов
+# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
-
-# Инициализация бота
-bot = Bot(token=config.tokenbot)
-dp = Dispatcher(bot)
-
-# Эхо
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+# Объект бота
+bot = Bot(token = config.BOT_TOKEN)
+# Диспетчер
+dp = Dispatcher()
 
 
-# запускаем лонг поллинг
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+@dp.message(Command("test2"))
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+
+
+@dp.message(Command("answer"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Простой ответ")  
+
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
+    
+    
